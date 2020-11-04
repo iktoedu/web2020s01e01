71491c136e6dbd16173f3fc39531db5f1dca7e1d
Maxim Polishchuk <mpolishchuck@vu.cdu.edu.ua>
2020-11-04 08:57:20 +0200

    Add basic webpack setup

---
+++ sources/js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const menu_module = require('menu');
+
+document.addEventListener('DOMContentLoaded', () => {
+    menu_module('.menu-trigger', '.menu');
+});
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: __dirname + '/sources/js/index.js',
+    },
+    output: {
+        path: __dirname + '/js',
+        filename: '[name].bundle.js',
+    },
+    resolve: {
+        modules: [
+            __dirname + '/sources/js/modules',
+        ]
+    }
+};
